4292638421a6fc4452149a76c8dbf69f80050ac4
Marttins <martinsjoao567@gmail.com>
2026-06-18 23:12:19 +0100

    Update shared.ts

--- lib/shared.ts
+++ lib/shared.ts
@@ -6,6 +6,6 @@ export const docsContentRoute = '/llms.mdx/docs';
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: 'Marttins011',
-  repo: 'mt-script-docs-v2',
+  repo: 'mt-scripts-docs-v2',
   branch: 'main',
 };
